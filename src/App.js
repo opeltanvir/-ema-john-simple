a66623926e35c6dc0209e0ca06eab369cefbd5f9
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-08 23:46:48 +0600

    ema

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/shop/shop';
 function App() {
   return (
     <div>
      <Header></Header>
-
+     <shop></shop>
     </div>
   );
 }
---
+++ src/components/shop/shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const shop = () => {
+    return (
+        <div>
+            <h1>This is shop</h1>
+        </div>
+    );
+};
+
+export default shop;
