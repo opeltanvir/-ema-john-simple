4e65d5a4dc7887e06f27609e14fdced17429878e
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-10 22:04:36 +0600

    ema

---
+++ src/components/Product/product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const product = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default product;
--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -2,18 +2,20 @@ import React from 'react';
 import fakeData from '../../fakeData';
 import { useState } from 'react';
 import './Shop.css';
+import Product from '../Product/product';
 
 const Shop = () => {
     const first10 = fakeData.slince(0,10);
     const [products,setProducts] = useState(first10);
+
+
     return (
         <div className = "shop-container">
              <div className = "product-container">
-             <ul>
                 {
-                    products.map(product => <li>{product.name}</li>)
+                    products.map(product => <product product={product}>{product.name}</product>)
                 }
-            </ul>
+            
             </div>
             <div className = "cart-container">
                 <h3>This is cart</h3>
