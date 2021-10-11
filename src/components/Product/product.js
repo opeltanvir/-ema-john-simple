270cc784e9ad0958f3460034b29db4932a94885d
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-11 22:04:15 +0600

    ema

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './product.css'
 
-const product = () => {
+const product = (props) => {
+    const {img, name ,price} = props.product;
     return (
-        <div>
+        <div class="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller} </small> </p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock-Order soon </small> </p>
+            </div>
+
             
         </div>
     );
