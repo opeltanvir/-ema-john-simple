6a6a23c2a9cf064eaf5803c9d8e71e6a2471db08
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-15 09:43:44 +0600

    ema

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -26,6 +26,10 @@ const Shop = () => {
             </div>
             <div className = "cart-container">
                 <h1>This is cart</h1>
+                <h2>This is cart</h2>
+                <h3>This is my</h3>
+
+
             </div>
           
         </div>
